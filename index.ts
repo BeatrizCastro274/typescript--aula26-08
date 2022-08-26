4efa5f71edeb85506f2c6f4e1b7d30ef3e119d6d
BeatrizCastro274 <biawolf4@hotmail.com>
2022-08-26 20:58:32 -0300

    Erros

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-let title: string = 'Tabata Timer - Aula 19/08';
+//let title: string = 'Tabata Timer - Aula 19/08';
+let title: string = 'Olá mundo!';
 document.getElementById('app.title').innerHTML = title;
 
 //let counter = 0;
@@ -54,6 +55,88 @@ appDivEnum.innerHTML = `
 <h1>Any variable</h1>
 <h2>${notSure}</h2>
 `;
+
+function warnUser(): void {
+  console.log('this is my warning message');
+}
+let myFunction: void = warnUser();
+
+let unusable: void = undefined;
+unusable = null;
+
+myFunction;
+
+const appDivNull: HTMLElement = document.getElementById('app-Null');
+appDivNull.innerHTML = `
+<h1>Void</h1>
+<h2> unusable: ${unusable}</h2>
+<h2>My Function: ${myFunction}</h2>
+`;
+
+//declare function create(o: object | null): void;
+//create({ prop: 0 }); //OK
+//create(null); //ok
+//create(42); //erro
+//create('String'); //erro
+//create(false); //erro
+//create(undefined); // OK
+//const appDivObject: HTMLElement = document.getElementById('app-Obj');
+//appDivObject.innerHTML = `
+//<h1>Object</h1>
+//`;
+
+let someValue: any = 'This is a string';
+let strLength: number = (<string>someValue).length;
+let someValue2: any = 'This is a string';
+let strLength2: number = (someValue2 as string).length;
+
+const appDivType: HTMLElement = document.getElementById('app-Type');
+appDivType.innerHTML = `
+<h1>Type assertations</h1>
+`;
+
+function sum(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+function fullName(pessoa: { name: string; lastname: string }): string {
+  return pessoa.name + ' ' + pessoa.lastname;
+}
+
+const appDivFunc: HTMLElement = document.getElementById('app-Func');
+appDivFunc.innerHTML = `
+<h1>Functions</h1>
+<h2> Soma de 2 e 3: ${sum(2, 3)}</h2>
+<h2> Soma de 11 e 329: ${sum(11, 329)}</h2>
+<h2> Meu nome completo: ${fullName({
+  lastname: 'Araujo',
+  name: 'Beatriz C.',
+})}</h2>
+`;
+
+//Import stylesheet
+import './style.css';
+
+let paragraph: string = 'Blablabla...';
+
+let counter = 0;
+
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  const appP: HTMLElement = document.getElementById('app.p');
+  appP.innerHTML = 'Contador:' + counter;
+}, 1000);
+
+const btn = document.getElementById('app.btn');
+btn?.addEventListener('click', alertMethod);
+function alertMethod(this: HTMLElement, ev: Event) {
+  alert('Alertando com Typescript');
+}
+
+// Write TypeScript code!
+
+const appH1: HTMLElement = document.getElementById('app.h1');
+appH1.innerHTML = title;
 
 // Aula 19/08
 //let intervalId = setInterval(() => {
